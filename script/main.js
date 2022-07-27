35fb63c060e01dcad26ee49351cc22ad609fe19f
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-07-27 16:54:50 +0200

    add Top Selling Section

---
+++ script/main.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".boxes", {
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        450: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+        1446: {
+            slidesPerView: 5,
+        },
+    },
+});
